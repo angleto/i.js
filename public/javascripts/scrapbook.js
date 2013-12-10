cccf56763e1da56339ad7183646d13e1d2ab5192
Mikhail Ksenzov <mikhail.ksenzov@gmail.com>
2013-12-10 00:34:34 -0500

    scrapbook.js event handling cleanup
    Minor cleanup: extracted a couple of methds for the keydown(…) event
    handler.

--- public/javascripts/scrapbook.js
+++ public/javascripts/scrapbook.js
@@ -12,56 +12,61 @@
         clone.find('.cell-input').focus().autosize();
     }
 
-    appendCell();
+    function insertTab(target) {
+        var start = target.selectionStart;
+        var end = target.selectionEnd;
 
+        var ident = "    ";
+        var newCaretPosition = start + ident.length;
 
+        var textarea = $(target);
+        var text = textarea.val().substring(0, start) + ident + textarea.val().substring(end);
+        textarea.val(text);
+
+        target.selectionStart = newCaretPosition;
+        target.selectionEnd = newCaretPosition;
+        target.focus();
+    }
+
+    function evalCell(target) {
+        var textarea = $(target);
+        var cell = textarea.parents('.cell');
+        var output = cell.find('.cell-output')
+        var js = textarea.val();
+        console.log(js);
+        $.ajax({
+            url: '/repl',
+            data: {'js': js, 'id': location.pathname.split('/').slice(-1)[0]},
+            type: 'post'
+        })
+            .done(function (data) {
+                output.text(data);
+                output.css('display', '');
+                var currentId = parseInt(cell.attr('id').replace(/^cell/, ''));
+                console.log('currentId: ' + currentId);
+                console.log('cellId: ' + cellId);
+                if (currentId === cellId) {
+                    appendCell();
+                }
+            })
+            .fail(function () {
+                console.log("error!");
+            });
+    }
 
     $('#document').keydown(function (e) {
         if ($(e.target).hasClass('cell-input')) {
             if (e.keyCode == 9) {
                 // Tab was pressed: ident
                 e.preventDefault();
-
-                var textarea = $(e.target);
-                var start = e.target.selectionStart;
-                var end =  e.target.selectionEnd;
-
-                var ident = "    ";
-                var newCaretPosition = start + ident.length;
-
-                var text = textarea.val().substring(0, start) + ident + textarea.val().substring(end);
-                textarea.val(text);
-
-                e.target.selectionStart = newCaretPosition;
-                e.target.selectionEnd = newCaretPosition;
-                e.target.focus();
+                insertTab(e.target);
             } else if (e.keyCode == 13 && event.shiftKey) {
-                // Shift+Enter was pressed: eval
+                // Shift+Enter was pressed: eval the cell content
                 e.preventDefault();
-                var textarea = $(e.target);
-                var cell = textarea.parents('.cell');
-                var output = cell.find('.cell-output')
-                var js = textarea.val();
-                console.log(js);
-                $.ajax({
-                    url: '/repl',
-                    data: {'js': js, 'id': location.pathname.split('/').slice(-1)[0]},
-                    type: 'post'
-                })
-                    .done(function (data) {
-                        output.text(data);
-                        output.css('display', '');
-                        var currentId = parseInt(cell.attr('id').replace(/^cell/, ''));
-                        console.log('currentId: ' + currentId);
-                        console.log('cellId: ' + cellId);
-                        if (currentId === cellId) {
-                            appendCell();
-                        }
-                    })
-                    .fail(function () {
-                        console.log("error!");
-                    });
+                evalCell(e.target);
             }
         }
-    })
+    });
+
+    appendCell();
 })();
